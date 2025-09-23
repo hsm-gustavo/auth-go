628bf39893a1e023cfa210bf4a55e1a33b5e48e4
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2025-09-23 08:30:03 -0300

    fix: migrations not working properly
    - modify migrate.go to use absolute path instead of relative
    
    - rename the *_up.sql to *.up.sql

--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"log"
+	"path/filepath"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/mysql"
@@ -10,8 +11,13 @@ import (
 
 func RunMigrations(dsn string) {
 	// fix later
+	migrationsPath, err := filepath.Abs("./internal/db/migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	m, err := migrate.New(
-		"file://migrations/",
+		"file://" + migrationsPath,
 		"mysql://"+dsn,
 	)
 	if err != nil {
